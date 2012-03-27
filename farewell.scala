c6d66eaf673956d3451c619ac80101e20e417e70
ehonlia <hongxin.liang@ericsson.com>
2012-03-27 10:16:03 +0800

    shorter

--- farewell.scala
+++ farewell.scala
@@ -2,13 +2,13 @@
 exec scala "$0" "$@"
 !#
 
-val KEY = "i hope in the next ten years there would be no other farewell letter brilliant than this one";
+val Key = "i hope in the next ten years there would be no other farewell letter brilliant than this one";
 
 val encrypted = scala.io.Source.fromFile(args(0)).mkString.replaceAll("\\s", "").split(",")
 val sb = new StringBuilder
 
-for (i <- 0 until encrypted.length) {
-  sb.append((encrypted(i).toInt ^ KEY(i % KEY.length)).asInstanceOf[Char])
+for (i <- encrypted.indices) {
+  sb.append((encrypted(i).toInt ^ Key(i % Key.length)).asInstanceOf[Char])
 }
 
 print(sb)
